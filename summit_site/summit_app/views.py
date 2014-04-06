79d65bd405afe22535548dbf92f01069d8695823
nielsbantilan <niels.bantilan@gmail.com>
2014-04-05 22:59:26 -0400

    WOOHOO

--- summit_site/summit_app/views.py
+++ summit_site/summit_app/views.py
@@ -5,15 +5,9 @@ from summit_app.models import Content
 
 
 def index(request):
-<<<<<<< HEAD
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'summit_app/index', context)
-=======
     latest_content_list = Content.objects.order_by('-pub_date')[:5]
     context = {'latest_content_list': latest_content_list}
     return render(request, 'summit_app/index.html', context)
->>>>>>> 21393d7e39249ad22588b92d41c959c37fcd0b44
 
 
 def detail(request, content_id):
